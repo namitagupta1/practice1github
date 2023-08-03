46f6bb3c8c5244c8d822fba3289a7c48900c96c7
namitagupta1 <141289991+namitagupta1@users.noreply.github.com>
2023-08-03 13:03:47 -0400

    Update practice1.java

--- src/forGITpractice/practice1.java
+++ src/forGITpractice/practice1.java
@@ -7,6 +7,7 @@ public class practice1 {
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
 	System.out.println("Myfirstproject");
+	System.out.println("Myfirstchange");		
 		}
 
 }
