d29963190b81db71b6d894632252c4d233100c1c
namit <namit@DESKTOP-5LQV91G>
2023-08-03 12:28:48 -0400

    committed

---
+++ src/forGITpractice/practice1.java
@@ -0,0 +1,12 @@
+package forGITpractice;
+
+public class practice1 {
+
+	
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	System.out.println("Myfirstproject");
+		}
+
+}
---
+++ src/forGITpractice/practice2.java
@@ -0,0 +1,23 @@
+package forGITpractice;
+
+public class practice2 {
+	public static void main(String[] args) {
+
+		 int i=1;
+		int n=0;
+		do {
+			 //n =sum of numbers
+			 n= n+i;
+			 System.out.println("i=" +i); 
+			 i++;
+			 
+		}
+	while( i <=50); 
+		System.out.println(" n =" +n);
+		//average = n/50
+		float avg = n/50f ;
+		System.out.println("avg =" +avg);
+	}
+
+}
+
